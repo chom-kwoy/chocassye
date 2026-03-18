1224a7e0257bcf69e934236660cce659e42fe086
Chomkwoy <chom.kwoy@xn--gt1b.xyz>
2026-03-18 17:02:47 +0900

    fix directory

---
+++ src/app/hanja/page.tsx
@@ -0,0 +1,9 @@
+import { Grid } from "@mui/material";
+
+export default async function HanjaPage() {
+  return (
+    <Grid container spacing={{ xs: 0.5, sm: 1 }} alignItems="center">
+      Hello
+    </Grid>
+  );
+}
